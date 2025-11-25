082b53e51d56a7d0ee6309433c77cecdb267ddae
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2025-11-25 10:11:11 +0100

    Adaptation du model pour une API de Concessionnaire Auto/Moto

--- blogData/models.py
+++ blogData/models.py
@@ -1,18 +1,23 @@
-from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, CASCADE
+from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, CASCADE, IntegerField, DecimalField
+from django.core.validators import RegexValidator
 from apiBlog import settings
 
 
-# Create your models here.
-class Article(Model):
-    title = CharField(max_length=512)
-    content = TextField()
-    author = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
+# Concessionnaire et Véhicule models
+class Concessionnaire(Model):
+    nom = CharField(max_length=64)
+    siret = CharField(max_length=14)
 
-class Comment(Model):
-    content = TextField()
-    author = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
-    article = ForeignKey(Article, on_delete=CASCADE, related_name='comments')
+class Vehicule(Model):
+    TYPE_MOTO = 'moto'
+    TYPE_AUTO = 'auto'
+    TYPE_CHOICES = [
+        (TYPE_MOTO),
+        (TYPE_AUTO),
+    ]
+
+    type = CharField(max_length=4, choices=TYPE_CHOICES)
+    marque = CharField(max_length=64)
+    chevaux = IntegerField()
+    prix_ht = DecimalField(max_digits=12, decimal_places=2)
+    concessionnaire = ForeignKey(Concessionnaire, on_delete=CASCADE, related_name='vehicules')
