883bef25af028737839ce6e4d04731d804eb6c0c
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2025-11-26 14:38:11 +0100

    Update gitignore pour simplement exclure __pycache__ et leger update du models.py

--- ConcessionaireData/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConcessionaireDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ConcessionaireData'
--- README.md
+++
@@ -1,4 +0,0 @@
-La base SQLite est fourni avec 2 users : 
-
-- userA (mot de passe **admin123**)
-- userB (mot de passe **test456**)
